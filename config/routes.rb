9d8d29766a40ec668386b681bc267d68e4f2f89a
Parmi89 <parmi_89@hotmail.com>
2023-07-13 18:02:15 -0300

    Se mejoro la vista del menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :frequentquestions
+  root 'landing#index'
   get 'admin/index'
   devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_for :admins, controllers: {sessions: 'admins/sessions'}
 
-  root 'landing#index'
 
+  resources :frequentquestions
+    
 end
